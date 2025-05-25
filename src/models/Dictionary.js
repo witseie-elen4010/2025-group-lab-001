d413b8edc0f86d7d55f4a8d20a860c730f530951
Nnete <nneteteka@gmail.com>
2025-05-25 18:01:37 +0200

    feat: Added prompts to the dictionary model - generated prompts for various tasks - added functionality to select which dictionary to use

--- src/models/Dictionary.js
+++ src/models/Dictionary.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // Private
-const dictionary = [
+const wordDictionary = [
   {
     civilian: 'Hotel',
     imposter: 'Motel'
@@ -32,11 +32,55 @@ const dictionary = [
   }
 ]
 
+const promptDictionary = [
+  {
+    civilian: 'you caught the bus right on time this morning.',
+    imposter: 'you barely made it onto the train today.'
+  },
+  {
+    civilian: 'you had a delicious picnic by the lake.',
+    imposter: 'you ate sandwiches beside the river.'
+  },
+  {
+    civilian: 'you wore a gown to the formal event.',
+    imposter: 'you dressed up for the party last night.'
+  },
+  {
+    civilian: 'you fixed the car in your garage over the weekend.',
+    imposter: 'you worked on your bike in the driveway.'
+  },
+  {
+    civilian: 'you adopted a puppy from the shelter.',
+    imposter: 'you rescued a kitten last month.'
+  },
+  {
+    civilian: 'you ordered sushi from the new Japanese place.',
+    imposter: 'you spent the weekend in a cabin on a mountain.'
+  },
+  {
+    civilian: 'you took a nap after lunch because you were exhausted.',
+    imposter: 'you rested in bed all morning since you felt tired.'
+  }
+]
+
 // Public
 module.exports = {
-  getWordPair: function () {
-    const randomIndex = Math.floor(Math.random() * dictionary.length)
+  getWordPair: function (dictionaryType) {
+    if (dictionaryType === 'prompt') {
+      return this.getRandomPrompt()
+    } else if (dictionaryType === 'word') {
+      return this.getRandomWord()
+    } else {
+      throw new Error('Invalid dictionary type. Use "prompt" or "word".')
+    }
+  },
+  getRandomPrompt: function () {
+    const randomIndex = Math.floor(Math.random() * promptDictionary.length)
+    return promptDictionary[randomIndex]
+  },
+  getRandomWord: function () {
+    const randomIndex = Math.floor(Math.random() * wordDictionary.length)
 
-    return dictionary[randomIndex]
+    return wordDictionary[randomIndex]
   }
 }
